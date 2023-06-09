5bd2b048a65e896275763da8b649985ea44e7905
YukiUSAMI <136035896+YukiUSAMI@users.noreply.github.com>
2023-06-09 13:52:32 +0900

    Greetings added

--- DiceGame.c
+++ DiceGame.c
@@ -7,6 +7,12 @@
 
 int main(){
 
+    char name[1000];
+  int count = 0;
+  printf("What is your name? \n");
+  scanf("%s",name)
+  printf("Hello, %s!\n",name);
+  
   int dice ;
   int sum = 0;
   printf("Rolling dice...\n");
