4a5908de6d4bdd8c1b6cec47562e8e2bd6e73e35
YukiUSAMI <136035896+YukiUSAMI@users.noreply.github.com>
2023-06-09 12:03:08 +0900

    Greetings added

--- DiceGame.c
+++ DiceGame.c
@@ -5,7 +5,16 @@
 #define MIN 1
 #define RANGE ((MAX)-(MIN)+1)
 
+
+
+
 int main(){
+  char name[1000];
+  int count = 0;
+  printf("What is your name? \n");
+  scanf("%s",name)
+  printf("Hello, %s!\n",name);
+  
   int dice ;
   int sum = 0;
   printf("Rolling dice...\n");
